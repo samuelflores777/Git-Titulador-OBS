e7ccb54a29518f3c660b8bf2054879f99f82958f
Samuel Flores <101888019+samuelflores777@users.noreply.github.com>
2023-07-04 20:19:58 -0500

    Agregar archivos de proyecto.

---
+++ SAMNET/Form1.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        Label8 = New Label()
+        TextBox7 = New TextBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        TabPage2 = New TabPage()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Button4 = New Button()
+        TabPage3 = New TabPage()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox4 = New TextBox()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Button6 = New Button()
+        Button3 = New Button()
+        Label7 = New Label()
+        Button8 = New Button()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        TabPage3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button2
+        ' 
+        Button2.FlatAppearance.MouseDownBackColor = Color.Lime
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button2.Location = New Point(21, 75)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(421, 73)
+        Button2.TabIndex = 0
+        Button2.Text = "ENVIAR"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(21, 46)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(421, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Controls.Add(TabPage3)
+        TabControl1.Location = New Point(12, 57)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(480, 407)
+        TabControl1.TabIndex = 3
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.BackColor = Color.PaleTurquoise
+        TabPage1.Controls.Add(Label8)
+        TabPage1.Controls.Add(TextBox7)
+        TabPage1.Controls.Add(Button1)
+        TabPage1.Controls.Add(Label1)
+        TabPage1.Controls.Add(TextBox1)
+        TabPage1.Controls.Add(Button2)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(472, 379)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "BLOQUE 1"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(21, 192)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(54, 15)
+        Label8.TabIndex = 7
+        Label8.Text = "TEXTO 2"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(21, 210)
+        TextBox7.Multiline = True
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(421, 39)
+        TextBox7.TabIndex = 6
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatAppearance.MouseDownBackColor = Color.Lime
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(21, 255)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(421, 73)
+        Button1.TabIndex = 5
+        Button1.Text = "ENVIAR"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(21, 28)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "TEXTO 1"
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.BackColor = Color.LightSalmon
+        TabPage2.Controls.Add(TextBox3)
+        TabPage2.Controls.Add(TextBox2)
+        TabPage2.Controls.Add(Label3)
+        TabPage2.Controls.Add(Label2)
+        TabPage2.Controls.Add(Button4)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(472, 379)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "BLOQUE 2"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(45, 142)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(396, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(45, 75)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(396, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(45, 124)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(54, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "TEXTO 2"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(45, 57)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "TEXTO 1"
+        ' 
+        ' Button4
+        ' 
+        Button4.FlatAppearance.MouseDownBackColor = Color.Lime
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button4.Location = New Point(45, 184)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(396, 72)
+        Button4.TabIndex = 0
+        Button4.Text = "ENVIAR"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.BackColor = Color.Purple
+        TabPage3.Controls.Add(TextBox6)
+        TabPage3.Controls.Add(TextBox5)
+        TabPage3.Controls.Add(TextBox4)
+        TabPage3.Controls.Add(Label6)
+        TabPage3.Controls.Add(Label5)
+        TabPage3.Controls.Add(Label4)
+        TabPage3.Controls.Add(Button6)
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Padding = New Padding(3)
+        TabPage3.Size = New Size(472, 379)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "BLOQUE 3"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(33, 193)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(407, 23)
+        TextBox6.TabIndex = 7
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(32, 121)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(408, 23)
+        TextBox5.TabIndex = 6
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(33, 55)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(407, 23)
+        TextBox4.TabIndex = 5
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.ForeColor = SystemColors.Control
+        Label6.Location = New Point(33, 175)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(54, 15)
+        Label6.TabIndex = 4
+        Label6.Text = "TEXTO 3"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.ForeColor = SystemColors.Control
+        Label5.Location = New Point(33, 103)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(54, 15)
+        Label5.TabIndex = 3
+        Label5.Text = "TEXTO 2"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.ForeColor = SystemColors.Control
+        Label4.Location = New Point(33, 37)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(54, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "TEXTO 1"
+        ' 
+        ' Button6
+        ' 
+        Button6.FlatAppearance.MouseDownBackColor = Color.Lime
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button6.ForeColor = SystemColors.Control
+        Button6.Location = New Point(32, 241)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(408, 76)
+        Button6.TabIndex = 0
+        Button6.Text = "ENVIAR"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.DeepSkyBlue
+        Button3.FlatAppearance.BorderColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        Button3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.ForeColor = SystemColors.ButtonHighlight
+        Button3.Location = New Point(12, 470)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(285, 30)
+        Button3.TabIndex = 4
+        Button3.Text = "SALIR"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 18F, FontStyle.Bold Or FontStyle.Underline, GraphicsUnit.Point)
+        Label7.ForeColor = SystemColors.Control
+        Label7.Location = New Point(115, 18)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(318, 32)
+        Label7.TabIndex = 5
+        Label7.Text = "TEXTOS ENVIOS - SAMNET"
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.RoyalBlue
+        Button8.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button8.ForeColor = SystemColors.Control
+        Button8.Location = New Point(343, 474)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(149, 23)
+        Button8.TabIndex = 6
+        Button8.Text = "CREAR DIRECTORIO"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.RoyalBlue
+        ClientSize = New Size(510, 505)
+        Controls.Add(Button8)
+        Controls.Add(Label7)
+        Controls.Add(Button3)
+        Controls.Add(TabControl1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "ENVIAR TEXTO - SAMNET"
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage1.PerformLayout()
+        TabPage2.ResumeLayout(False)
+        TabPage2.PerformLayout()
+        TabPage3.ResumeLayout(False)
+        TabPage3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ SAMNET/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim ruta As String = "c:/samueltxt/"
+    Dim archivo1 As String = "B1txt1.txt"
+    Dim archivo12 As String = "B1txt2.txt"
+    Dim archivo21 As String = "B2txt1.txt"
+    Dim archivo22 As String = "B2txt2.txt"
+    Dim archivo31 As String = "B3txt1.txt"
+    Dim archivo32 As String = "B3txt2.txt"
+    Dim archivo33 As String = "B3txt3.txt"
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim escribir As New StreamWriter(ruta & archivo1)
+            escribir.WriteLine(TextBox1.Text)
+            escribir.Close()
+        Catch ex As Exception
+            MsgBox("DIRECTORIO NO EXISTE", MsgBoxStyle.Critical, ":::SAMNET:::")
+        End Try
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Dim escribir21 As New StreamWriter(ruta & archivo21)
+            Dim escribir22 As New StreamWriter(ruta & archivo22)
+            escribir21.WriteLine(TextBox2.Text)
+            escribir22.WriteLine(TextBox3.Text)
+            escribir21.Close()
+            escribir22.Close()
+        Catch ex As Exception
+            MsgBox("DIRECTORIO NO EXISTE", MsgBoxStyle.Critical, ":::SAMNET:::")
+        End Try
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Try
+            Dim escribir31 As New StreamWriter(ruta & archivo31)
+            Dim escribir32 As New StreamWriter(ruta & archivo32)
+            Dim escribir33 As New StreamWriter(ruta & archivo33)
+            escribir31.WriteLine(TextBox4.Text)
+            escribir32.WriteLine(TextBox5.Text)
+            escribir33.WriteLine(TextBox6.Text)
+            escribir31.Close()
+            escribir32.Close()
+            escribir33.Close()
+        Catch ex As Exception
+            MsgBox("DIRECTORIO NO EXISTE", MsgBoxStyle.Critical, ":::SAMNET:::")
+        End Try
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Try
+
+            If File.Exists(ruta) Then
+                MsgBox("EXISTE", MsgBoxStyle.Information, ":::SAMNET:::")
+            Else
+                Directory.CreateDirectory(ruta)
+                MsgBox("DIRECTORIO CREADO EN c:samueltxt ", MsgBoxStyle.Information, ":::SAMNET:::")
+            End If
+        Catch ex As Exception
+            MsgBox("Se presento un problema al momento de crear el archivo: " & ex.Message, MsgBoxStyle.Critical, ":::SAMNET:::")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim escribir As New StreamWriter(ruta & archivo12)
+            escribir.WriteLine(TextBox7.Text)
+            escribir.Close()
+        Catch ex As Exception
+            MsgBox("DIRECTORIO NO EXISTE", MsgBoxStyle.Critical, ":::SAMNET:::")
+        End Try
+    End Sub
+End Class
